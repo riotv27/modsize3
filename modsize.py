370b2a89886f8aa918d28207cc73690d244ac645
riotv27 <93431512+riotv27@users.noreply.github.com>
2022-09-25 10:22:51 +0700

    Update modsize.py

--- modsize.py
+++ modsize.py
@@ -12,9 +12,7 @@ parser.add_argument("--height", "-sh", type=int, help="New height of image")
 args = parser.parse_args()
 
 
-
 def modify_file(offset1, offset2, filename,output, width=None, height=None):
-	print("Loading")
 	bin_arr = [] 
 	with open(filename,'rb') as f:
 		arr = f.read()
@@ -24,8 +22,6 @@ def modify_file(offset1, offset2, filename,output, width=None, height=None):
 	org_width = hex(int(bin_arr[offset1].hex() + bin_arr[offset1+1].hex(),16))
 	org_height = hex(int(bin_arr[offset2].hex() + bin_arr[offset2+1].hex(),16))
 
-
-	
 	print("Detected width: %d px" % int(org_width,16))
 	print("Detected height: %d px" % int(org_height,16))
 
@@ -38,7 +34,6 @@ def modify_file(offset1, offset2, filename,output, width=None, height=None):
 	if height == None:
 		height = int(org_height,16)
 	
-
 	new_width=str(hex(width))[2:].zfill(4) #Width bad adaptive filter value :(/
 	new_height=str(hex(height))[2:].zfill(4)
 
@@ -55,11 +50,10 @@ def modify_file(offset1, offset2, filename,output, width=None, height=None):
 	bin_arr[offset2]=bytes.fromhex(str(new_height)[:2])
 	bin_arr[offset2+1]=bytes.fromhex(str(new_height)[2:])
 
-	
-	
 	with open(output, "wb") as binary_file:
 		binary_file.write(b"".join(bin_arr))
 	print("Image saved!")
+	
 	
 def modify_png(filename,output,width,height):
 	modify_file(18,22, filename,output,width,height)
@@ -74,13 +68,14 @@ def modify_jpg(filename,output,width,height):
 	i = 0
 	for b in arr:
 		b = bytes([b])
-		if re.match("ffc[0-3]",prev + b.hex()):
+		if re.match("ffc[0-3]", prev + b.hex()):
 			break
 		i+=1
 		prev = b.hex()
 	print("Found magic bytes on offset %d " % i)
 	modify_file(i+6,i+4,filename,output,width,height)
 
+	
 def process_file(filename,output,width,height):
 	kind = filetype.guess(filename)
 	if kind is None:
@@ -90,7 +85,7 @@ def process_file(filename,output,width,height):
 		print("Detected: png")
 		modify_png(filename,output,width,height)
 	elif kind.mime == "image/jpeg":
-		print("Detected: jpg")
+		print("Detected: jpg/jpeg")
 		modify_jpg(filename,output,width,height)
 	else:
 		print("Filetype not supported")
